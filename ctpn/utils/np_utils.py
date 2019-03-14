526ceec7401b72fffe0903f068de5390c839cd36
mick.yi <mick.yi@embracesource.com>
2019-03-14 21:47:24 +0800

    numpy工具类

--- ctpn/utils/np_utils.py
+++ ctpn/utils/np_utils.py
@@ -67,6 +67,27 @@ def compute_iou(boxes_a, boxes_b):
     return iou
 
 
+def compute_iou_1vn(box, boxes, box_area, boxes_area):
+    """Calculates IoU of the given box with the array of the given boxes.
+    box: 1D vector [y1, x1, y2, x2]
+    boxes: [boxes_count, (y1, x1, y2, x2)]
+    box_area: float. the area of 'box'
+    boxes_area: array of length boxes_count.
+
+    Note: the areas are passed in rather than calculated here for
+    efficiency. Calculate once in the caller to avoid duplicate work.
+    """
+    # Calculate intersection areas
+    y1 = np.maximum(box[0], boxes[:, 0])
+    y2 = np.minimum(box[2], boxes[:, 2])
+    x1 = np.maximum(box[1], boxes[:, 1])
+    x2 = np.minimum(box[3], boxes[:, 3])
+    intersection = np.maximum(x2 - x1, 0) * np.maximum(y2 - y1, 0)
+    union = box_area + boxes_area[:] - intersection[:]
+    iou = intersection / union
+    return iou
+
+
 def threshold(coords, min_, max_):
     return np.maximum(np.minimum(coords, max_), min_)
 
@@ -81,3 +102,55 @@ def clip_boxes(boxes, im_shape):
     boxes[:, 0::2] = threshold(boxes[:, 0::2], 0, im_shape[1])
     boxes[:, 1::2] = threshold(boxes[:, 1::2], 0, im_shape[0])
     return boxes
+
+
+def non_max_suppression(boxes, scores, iou_threshold):
+    """
+    非极大抑制
+    :param boxes: [n,(y1,x1,y2,x2)]
+    :param scores: [n]
+    :param iou_threshold:
+    :return:
+    """
+    assert boxes.shape[0] > 0
+    if boxes.dtype.kind != "f":
+        boxes = boxes.astype(np.float32)
+
+    # Compute box areas
+    y1 = boxes[:, 0]
+    x1 = boxes[:, 1]
+    y2 = boxes[:, 2]
+    x2 = boxes[:, 3]
+    area = (y2 - y1) * (x2 - x1)
+
+    # Get indicies of boxes sorted by scores (highest first)
+    ixs = scores.argsort()[::-1]
+
+    pick = []
+    while len(ixs) > 0:
+        # Pick top box and add its index to the list
+        i = ixs[0]
+        pick.append(i)
+        # Compute IoU of the picked box with the rest
+        iou = compute_iou_1vn(boxes[i], boxes[ixs[1:]], area[i], area[ixs[1:]])
+        # Identify boxes with IoU over the threshold. This
+        # returns indices into ixs[1:], so add 1 to get
+        # indices into ixs.
+        remove_ixs = np.where(iou > iou_threshold)[0] + 1
+        # Remove indices of the picked and overlapped boxes.
+        ixs = np.delete(ixs, remove_ixs)
+        ixs = np.delete(ixs, 0)
+    return np.array(pick, dtype=np.int32)
+
+
+def main():
+    x = np.zeros(shape=(0, 4))
+    y = pad_to_fixed_size(x, 5)
+    print(y.shape)
+    x = np.asarray([], np.float32).reshape((-1, 4))
+    y = pad_to_fixed_size(x, 5)
+    print(y.shape)
+
+
+if __name__ == '__main__':
+    main()
